ee64ea1be62837acb4bee575c37a25dbb8b63273
Ezequiel Oliveira <esofinney@gmail.com>
2021-03-18 02:07:22 -0300

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ require_once "environment.php";
 $config= array();
 
 if (ENVIRONMENT === 'development'){
-	define ("BASE_URL", "http://localhost/GitHub/hospital_system/web_medico/");
+	define ("BASE_URL", "http://localhost/GitHub/medical_system/");
 	$config['dbname'] = 'medical_system';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
